ea3a697f84863d28f3f0d19bf0dffd4702a16e77
richardimaoka <richard.s.imaoka@gmail.com>
2025-09-26 03:06:20 +0900

    lib.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
+import { printAA } from "./lib.js";
 import type { AA } from "./types.js";
 
 const aa: AA = { a: 10 };
 
-console.log("aloha", aa);
+printAA(aa);
---
+++ lib.ts
@@ -0,0 +1,5 @@
+import type { AA } from "./types";
+
+export function printAA(aa: AA) {
+  console.log("print", aa);
+}
